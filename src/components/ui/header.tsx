e50d186a765608f2efc7d2aae2ba5652d830b617
Yajat Gulati <50955708+sheldor07@users.noreply.github.com>
2024-01-02 12:18:19 +0530

    feat: added shadcn/ui config

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="py-4 px-3">
+      <h1 className="text-blue-500  bg-black scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        NTUMODS{" "}
+      </h1>
+    </header>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
